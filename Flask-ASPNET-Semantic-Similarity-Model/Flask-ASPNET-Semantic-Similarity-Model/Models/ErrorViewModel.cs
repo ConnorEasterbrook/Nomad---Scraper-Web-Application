0287e8cb5c67749fa7f0a4714761527dc290b9a1
Connor Easterbrook <connor@craigndave.co.uk>
2023-06-06 19:15:12 +0100

    Flask

--- Flask-ASPNET-Semantic-Similarity-Model/Flask-ASPNET-Semantic-Similarity-Model/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Flask_ASPNET_Semantic_Similarity_Model.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
