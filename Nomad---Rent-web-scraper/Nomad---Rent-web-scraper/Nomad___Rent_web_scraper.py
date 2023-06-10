0773621ce1396b0abac7c6ca26702490fdb62639
Connor <connoreaster@gmail.com>
2023-06-10 14:28:11 +0100

    Get and clean listing information from Rightmove

--- Nomad---Rent-web-scraper/Nomad---Rent-web-scraper/Nomad___Rent_web_scraper.py
+++ Nomad---Rent-web-scraper/Nomad---Rent-web-scraper/Nomad___Rent_web_scraper.py
@@ -1,5 +1,6 @@
 from os import write
 import random
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,7 +11,6 @@ class ZooplaScraper:
        response = self.fetch(url)
 
        html = ''
-
        with open('rightmove.html', 'r', encoding='utf-8') as html_file:
             for line in html_file:
                 html += html_file.read()
@@ -30,7 +30,33 @@ class ZooplaScraper:
         return response
 
     def parse(self, html):
-        print(html)
+        content = BeautifulSoup(html, 'lxml') 
+        titles = [title.text.replace('\n', '').replace(' ', '') for title in content.select( ".propertyCard-title")]
+
+        # Get and clean listing information for each property so we can show it
+        titles = [title.text for title in content.select( ".propertyCard-title")]
+        titles = [title.replace('\n', '') for title in titles]
+        titles = [re.sub(r'\s+', ' ', title) for title in titles]
+        titles = [title.replace('  ', '') for title in titles]
+        prices = [price.text for price in content.select(".propertyCard-priceValue")]
+
+        # Get the integer value of the price for each property so we can calculate with it
+        intPrices = []
+        for price in prices:
+            try:
+                tempPrice = re.search(r'\d+', price).group()
+                intPrices.append(int(tempPrice))
+            except ValueError:
+                print("Unable to convert price to integer.")
+
+        listings = []
+
+        for title, price in zip(titles, prices):
+            listings.append(f'{title}- {price}')
+
+
+        output = ",".join(listings)
+        print(output)
 
     
 if __name__ == '__main__':
